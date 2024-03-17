00f77539abae2dd57cc086d06af7d3f5855b161e
Xbc-gressor <2765024335@qq.com>
2024-03-17 22:23:16 +0800

    debug generic_smbo RandomAdvisor

--- test/optimizer/test_generic_smbo.py
+++ test/optimizer/test_generic_smbo.py
@@ -1,5 +1,4 @@
 import pytest
-from unittest.mock import MagicMock, patch
 from openbox.optimizer.base import BOBase
 from openbox.optimizer.generic_smbo import SMBO
 from openbox.utils.config_space import ConfigurationSpace
@@ -10,6 +9,19 @@ def test_smbo(configspace_tiny, func_brain):
     config_space = configspace_tiny
     objective_function = func_brain
     smbo = SMBO(objective_function, config_space, max_runs=2, initial_runs=1, logging_dir='test/datas')
+    assert isinstance(smbo, BOBase)
+    assert smbo.objective_function == objective_function
+    assert smbo.config_space == config_space
+
+    smbo.run()
+    assert smbo.iteration_id == 2
+    assert len(smbo.config_advisor.history) == 2
+
+
+def test_smbo_default(configspace_tiny, func_brain):
+    config_space = configspace_tiny
+    objective_function = func_brain
+    smbo = SMBO(objective_function, config_space, advisor_type='random', max_runs=2, initial_runs=1, logging_dir='test/datas')
     assert isinstance(smbo, BOBase)
     assert smbo.objective_function == objective_function
     assert smbo.config_space == config_space
